bf2bfeca2421eecb3d6f5b718bab000e00e36eee
Kamil Kuczaj <kamil.kuczaj@gmail.com>
2023-03-20 22:36:21 +0100

    Initial Commit

---
+++ GpsPolling/GpsPollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GpsPollingApp.swift
+//  GpsPolling
+//
+//  Created by Kamil Kuczaj on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GpsPollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

d786e8b3747d96c952375d813801ae061818d699
Kamil Kuczaj <kamil.kuczaj@gmail.com>
2023-03-20 23:08:07 +0100

    First iteration of collecting Location data.
    Thanks to ChatGPT

--- GpsPolling/ContentView.swift
+++ GpsPolling/ContentView.swift
@@ -6,16 +6,67 @@
 //
 
 import SwiftUI
+import CoreLocation
+
+class LocationViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    @Published var locationManager = CLLocationManager()
+    @Published var csvData: String = "Timestamp,Latitude,Longitude,Altitude\n"
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+            self.saveLocationData()
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        default:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func saveLocationData() {
+        if let location = locationManager.location {
+            let timestamp = Date().timeIntervalSince1970
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            let altitude = location.altitude
+            
+            let row = "\(timestamp),\(latitude),\(longitude),\(altitude)\n"
+            csvData.append(row)
+        }
+    }
+    
+    func saveCSVDataToFile() {
+        let fileName = "LocationData.csv"
+        let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(fileName)
+        
+        do {
+            try csvData.write(to: path!, atomically: true, encoding: String.Encoding.utf8)
+            print("CSV file saved: \(String(describing: path))")
+        } catch {
+            print("Failed to save CSV file: \(error)")
+        }
+    }
+}
 
 struct ContentView: View {
+    @StateObject private var locationViewModel = LocationViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Location Tracker")
+            .padding()
+            .onDisappear {
+                locationViewModel.saveCSVDataToFile()
+            }
     }
 }
 

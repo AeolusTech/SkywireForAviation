5ae5ab57a154f88bd4557bd01f437748e78c86de
Kamil Kuczaj <kamil.kuczaj@gmail.com>
2023-03-21 10:11:20 +0100

    Map added above the Label and button.

--- GpsPolling/ContentView.swift
+++ GpsPolling/ContentView.swift
@@ -6,7 +6,47 @@
 //
 
 import SwiftUI
+import MapKit
 import CoreLocation
+
+struct MapView: UIViewRepresentable {
+    @Binding var coordinate: CLLocationCoordinate2D
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        mapView.isScrollEnabled = false
+        mapView.isZoomEnabled = false
+        mapView.isRotateEnabled = false
+        mapView.isPitchEnabled = false
+        mapView.isUserInteractionEnabled = false
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        uiView.setCenter(coordinate, animated: true)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+        
+        uiView.removeAnnotations(uiView.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        uiView.addAnnotation(annotation)
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+    }
+}
 
 class LocationViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
     @Published var locationManager = CLLocationManager()
@@ -95,6 +135,9 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            MapView(coordinate: .constant(locationViewModel.locationManager.location?.coordinate ?? CLLocationCoordinate2D()))
+                .frame(height: 300)
+            
             Text("Location Tracker")
                 .font(.largeTitle)
                 .padding()
